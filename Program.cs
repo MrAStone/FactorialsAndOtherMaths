8f08fd8d8936dbd34d9d0ae1a85d55890cf56027
Al Stone <astone@heckgrammar.co.uk>
2025-01-21 13:57:39 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FactorialsAndOtherMaths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // n! (n factorial) is a mathematical term
+            // that multiplies all numbers up to and including n
+            // 2! = 2 * 1 = 2
+            // 3! = 3 * 2 * 1 = 6
+            // 5! = 5 * 4 * 3 * 2 * 1 = 125
+            // Write a program that will take a user input and output the factorial
+            // of that input
+            // e.g
+            // User enters 7
+            // output 5040
+
+            // Write the code so that your program outputs the factorial of the numbers
+            // 1 to 20 in the format shown below
+            /*    1! = 1
+                  2! = 2
+                  3! = 6
+                  4! = 24
+                  5! = 120
+                  ...
+            */
+            // what is the problem?
+            // how can you fix it?
+
+
+        }
+
+    }
+}
